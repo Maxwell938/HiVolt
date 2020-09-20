8b3918a182e39559c8376b874fb130af50c2824f
Maxwell938 <71574636+Maxwell938@users.noreply.github.com>
2020-09-20 16:31:57 -0700

    Update Mho.java

--- Mho.java
+++ Mho.java
@@ -14,67 +14,22 @@ public class Mho {
 	{
 		return arr[y+1][x+1] == value;
 	}
+	public double dis(int x, int y)
+	{
+		return Math.sqrt(x*x + y*y);
+	}
 	public void moves(int x1, int y1, int arr[][])
 	{
-		for (int i = 0; i <2; i++)
+		int dx = x1-x;
+		int dy = y1-y;
+		int tempx = x;
+		int tempy = y;
+		double slope = (double) (double)dy/(double)dx;
+		if (Math.abs(dx) == Math.abs(dy))
 		{
-			int dx = x1-x;
-			int dy = y1-y;
-			int tempx = x;
-			int tempy = y;
-			double slope = (double) (double)dy/(double)dx;
-			if (Math.abs(dx) == Math.abs(dy))
+			if (dx > 0)
 			{
-				if (dx > 0)
-				{
-					tempx++;
-					if (dy > 0)
-					{
-						tempy++;
-					}
-					else
-					{
-						tempy--;
-					}
-				}
-				else
-				{
-					tempx--;
-					if (dy > 0)
-					{
-						tempy++;
-					}
-					else
-					{
-						tempy--;
-					}
-				}
-				if (check(tempx,tempy,i,arr))
-				{
-					x = tempx;
-					y = tempy;
-					break;
-				}
-			} 
-			else if (Math.abs(dx) > Math.abs(dy))
-			{
-				if (dx > 0)
-				{
-					tempx++;
-				}
-				else
-				{
-					tempx--;
-				}
-				if (check(tempx,tempy,i,arr))
-				{
-					x = tempx;
-					y = tempy;
-					break;
-				}
-			}
-			else if (Math.abs(dx) < Math.abs(dy))
-			{
+				tempx++;
 				if (dy > 0)
 				{
 					tempy++;
@@ -83,13 +38,80 @@ public class Mho {
 				{
 					tempy--;
 				}
-				if (check(tempx,tempy,i,arr))
+			}
+			else
+			{
+				tempx--;
+				if (dy > 0)
 				{
-					x = tempx;
-					y = tempy;
-					break;
+					tempy++;
+				}
+				else
+				{
+					tempy--;
 				}
 			}
+			x = tempx;
+			y = tempy;
+			return;
+		} 
+		else if (Math.abs(dy) == 0)
+		{
+			if (dx > 0)
+			{
+				tempx++;
+			}
+			else
+			{
+				tempx--;
+			}
+			x = tempx;
+			y = tempy;
+			return;
 		}
+		else if (Math.abs(dx) == 0)
+		{
+			if (dy > 0)
+			{
+				tempy++;
+			}
+			else
+			{
+				tempy--;
+			}
+			x = tempx;
+			y = tempy;
+			return;
+		}
+		int dxx[] = new int[] {1,1,0,-1,-1,-1,0,1};
+		int dyy[] = new int[] {0,1,1,1,0,-1,-1,-1};
+		double dis[] = new double[8];
+		for (int i =0; i < 8; i++)
+		{
+			int tempx1 = x + dxx[i];
+			int tempy1 = y + dyy[i];
+			if (check(tempx1,tempy1,0,arr))
+			{
+				double dist = dis(tempx1-x, tempy1-y);
+				dis[i] = dist;
+			}
+			else
+			{
+				dis[i] = -2.0;
+			}
+		}
+		double min = Integer.MAX_VALUE;
+		int index = 0;
+		for (int i =0; i < 8; i++)
+		{
+			if (dis[i] < min && dis[i] > 0)
+			{
+				min = dis[i];
+				index = i;
+			}
+		}
+		x+=dxx[index];
+		y+=dyy[index];
+		return;
 	}
 }
