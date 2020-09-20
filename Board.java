c17b6976cbede7defad418d3e1077b9f31f574c0
Maxwell938 <71574636+Maxwell938@users.noreply.github.com>
2020-09-20 16:31:35 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,17 +3,26 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
-public class Board extends JFrame implements KeyListener{
+//import javax.sound.*;
+public class Board extends JFrame{ //implements KeyListener{
 	Random r= new Random();
 	JFrame f = new JFrame();
+	JFrame f2 = new JFrame();
+	JFrame f3 = new JFrame();
+	JFrame f4 = new JFrame();
+	JFrame thx = new JFrame();;
 	Mho Mhos[];
 	Player p1;
 	Fence fence[];
 	JLabel l;
+	JTextField txt;
+	JLabel input;
+	JPanel panel;
 	int arr[][] = new int[12][12];
 	public JButton arr2[][];
 	boolean gameover = false;
-	public Board()
+	public Board(){}
+	public void initallize()
 	{
 		arr2 = new JButton[12][12];
 		f.setSize(800, 800);
@@ -25,9 +34,22 @@ public class Board extends JFrame implements KeyListener{
 			for (int ii =0; ii <12; ii++)
 			{
 				arr2[i][ii] = new JButton();
-				//arr2[i][ii].setBackground(new Color(0,0,0));
-				arr2[i][ii].setBorderPainted(true);
+				arr2[i][ii].setBackground(new Color(255,255,255));
+				arr2[i][ii].setBorderPainted(false);
 				arr2[i][ii].setOpaque(true);
+				arr2[i][ii].addKeyListener(new KeyListener () {
+					@Override
+					public void keyPressed(KeyEvent e) {
+						String str = "";
+						str += e.getKeyChar();
+						str = str.toUpperCase();
+						process(str.charAt(0));
+					}
+					@Override
+					public void keyTyped(KeyEvent e) {}
+					@Override
+					public void keyReleased(KeyEvent e) {}
+				});
 				f.add(arr2[i][ii]);
 			}
 		}
@@ -80,9 +102,9 @@ public class Board extends JFrame implements KeyListener{
 			{
 				arr[y+1][x+1] = 2;
 				Mhos[i] = new Mho(x,y);
-				ImageIcon imageIcon2 = new ImageIcon("D:/Mho.png");
+				ImageIcon imageIcon2 = new ImageIcon("D:/Mho2.jpg");
 				Image image23 = imageIcon2.getImage();
-				Image image22 = image23.getScaledInstance(66, 66,  java.awt.Image.SCALE_SMOOTH);
+				Image image22 = image23.getScaledInstance(59, 59,  java.awt.Image.SCALE_SMOOTH);
 				arr2[y+1][x+1].setIcon(new ImageIcon(image22));
 			}
 		}
@@ -100,7 +122,7 @@ public class Board extends JFrame implements KeyListener{
 				p1= new Player(x,y);
 				ImageIcon imageIcon = new ImageIcon("D:/Player.jpg");
 				Image image = imageIcon.getImage();
-				Image image2 = image.getScaledInstance(59, 59,  java.awt.Image.SCALE_SMOOTH);
+				Image image2 = image.getScaledInstance(45, 59,  java.awt.Image.SCALE_SMOOTH);
 				arr2[y+1][x+1].setIcon(new ImageIcon(image2));
 			}
 		}
@@ -128,14 +150,23 @@ public class Board extends JFrame implements KeyListener{
 		p1.move(keyPressed);
 		if (deadP(p1.x+1,p1.y+1))
 		{
-			//gameover();
-			System.out.println("UR");
+			gameover();
 			return;
 		}
-		f.setTitle("Moves: " + p1.moves);
+		if (keyPressed == 'J')
+			System.out.println(arr[p1.y+1][p1.x+1]);
+		int count = 0;
+		for (int i =0; i <12; i++)
+		{
+			if (Mhos[i].alive)
+			{
+				count++;
+			}
+		}
+		f.setTitle("Moves: " + p1.moves + "; Mhos left: " + count + "/12");
 		ImageIcon imageIcon = new ImageIcon("D:/Player.jpg");
 		Image image = imageIcon.getImage();
-		Image image2 = image.getScaledInstance(59, 59,  java.awt.Image.SCALE_SMOOTH);
+		Image image2 = image.getScaledInstance(45, 59,  java.awt.Image.SCALE_SMOOTH);
 		arr2[p1.y+1][p1.x+1].setIcon(new ImageIcon(image2));
 		f.setVisible(true);
 		boolean alldead = true;
@@ -143,7 +174,6 @@ public class Board extends JFrame implements KeyListener{
 		{
 			if (Mhos[i].alive && keyPressed != 'J')
 			{
-				alldead= false;
 				arr[Mhos[i].y+1][Mhos[i].x+1] = 0;
 				arr2[Mhos[i].y+1][Mhos[i].x+1].setIcon(new ImageIcon());
 				Mhos[i].moves(p1.x, p1.y, arr);
@@ -152,43 +182,119 @@ public class Board extends JFrame implements KeyListener{
 					Mhos[i].alive = false;
 					continue;
 				}
-				ImageIcon imageIcon2 = new ImageIcon("D:/Mho.png");
+				ImageIcon imageIcon2 = new ImageIcon("D:/Mho2.jpg");
 				Image image23 = imageIcon2.getImage();
-				Image image22 = image23.getScaledInstance(66, 66,  java.awt.Image.SCALE_SMOOTH);
+				Image image22 = image23.getScaledInstance(59, 59,  java.awt.Image.SCALE_SMOOTH);
 				arr2[Mhos[i].y+1][Mhos[i].x+1].setIcon(new ImageIcon(image22));
 				arr[Mhos[i].y+1][Mhos[i].x+1] = 2;
 			}
 		}
+		for (int i =0; i < 12; i++)
+		{
+			if (Mhos[i].alive) alldead = false;
+		}
 		if (alldead || p1.moves >= 12)
 		{
-			System.out.println(p1.moves);
+			gamewin();
+			return;
 		}
 		f.setVisible(true);
 		if (deadP(p1.x+1,p1.y+1))
 		{
-			System.out.println("DEAD");
-			//gameover();
+			gameover();
 			return;
 		}
 	}
 	public void gameover()
 	{
 		gameover= true;
-	    add(l,BorderLayout.CENTER);
-	    ImageIcon img = new ImageIcon("gameover.jpg");
-	    l.setIcon(img);
+		f2.setSize(800, 800);
+		f2.setLayout(new GridLayout(1,1));
+		f2.setResizable(false);
+		JButton j = new JButton();
+		ImageIcon imageIcon2 = new ImageIcon("D:/gameover.jpg");
+		Image image23 = imageIcon2.getImage();
+		Image image22 = image23.getScaledInstance(800, 800,  java.awt.Image.SCALE_SMOOTH);
+	    ImageIcon img = new ImageIcon(image22);
+	    j.setIcon(img);
+	    f2.add(j);
+	    f.setVisible(false);
+	    f3.setVisible(false);
+	    f2.setVisible(true);
+	    ask();
 	}
-	@Override
-	public void keyPressed(KeyEvent e) {
-		char key = e.getKeyChar();
+	public void gamewin()
+	{
+		gameover= true;
+		f2.setSize(800, 800);
+		f2.setLayout(new GridLayout(1,1));
+		f2.setResizable(false);
+		JButton j = new JButton();
+		ImageIcon imageIcon2 = new ImageIcon("D:/Win.jpg");
+		Image image23 = imageIcon2.getImage();
+		Image image22 = image23.getScaledInstance(800, 800,  java.awt.Image.SCALE_SMOOTH);
+	    ImageIcon img = new ImageIcon(image22);
+	    j.setIcon(img);
+	    f2.add(j);
+	    f.setVisible(false);
+	    f3.setVisible(false);
+	    f2.setVisible(true);
+	    ask();
 	}
-	@Override
-	public void keyTyped(KeyEvent e) {
-		String str = "";
-		str += e.getKeyChar();
-		str.toUpperCase();
-		process(str.charAt(0));
+	public void ask()
+	{
+	    input = new JLabel("Do you want to play again? (y for yes and n for no)");
+	    txt = new JTextField(5);
+	    txt.addActionListener(new Listener());
+	    panel = new JPanel();
+	    panel.setPreferredSize (new Dimension(500, 500));
+        panel.setBackground (Color.white);
+        panel.add (input);
+        panel.add (txt);
+        f4.getContentPane().add(panel);
+        f4.pack();
+        f4.setVisible(true);
 	}
-	@Override
-	public void keyReleased(KeyEvent e) {}
+	class Listener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) {
+			String text = txt.getText();
+			if (text.equals("y"))
+			{
+				f.setVisible(false);
+				f2.setVisible(false);
+				f3.setVisible(false);
+				initallize();
+			}
+			else if(text.equals("n"))
+			{
+				JButton jj = new JButton();
+				ImageIcon imageIcon5 = new ImageIcon("D:/thx.jpg");
+				Image image5 = imageIcon5.getImage();
+				Image imagef = image5.getScaledInstance(800, 800,  java.awt.Image.SCALE_SMOOTH);
+			    ImageIcon img = new ImageIcon(imagef);
+			    jj.setIcon(new ImageIcon());
+				thx.setSize(800, 800);
+				thx.setLayout(new GridLayout(1,1));
+				thx.setResizable(false);
+			    thx.add(jj);
+			    f.setVisible(false);
+			    f2.setVisible(false);
+			    f3.setVisible(false);
+			    f4.setVisible(false);
+			    thx.setVisible(true);
+				try {
+					Thread.sleep(4000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			    System.exit(0);
+			}
+			else
+			{
+				input = new JLabel("Invalid input. Try again. (y for yes and n for no)");
+			}
+		}
+	}
 }
